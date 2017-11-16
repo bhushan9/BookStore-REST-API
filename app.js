b34a37a606992cc006d133c705ce9b0c7c432ed1
bhushan9 <bhushan.crysis@gmail.com>
2017-11-16 17:07:33 -0500

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ app.use(express.static(__dirname+'/client'));
 app.use(bodyParser.json());
 
 app.use(function(req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+	res.setHeader('Access-Control-Allow-Origin', 'http://159.203.166.19:4200');
+//     res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
     // res.setHeader('Access-Control-Allow-Origin', 'http://localhost:7020');
   	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   	res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS');
